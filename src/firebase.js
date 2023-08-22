597158de9f1e6b43ca24a9079a605deb8d0c3bc5
MUHAMMAD AIZAZ <aizaznadeem174@gmail.com>
2023-08-22 14:29:45 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -6,14 +6,14 @@ import { getFirestore } from 'firebase/firestore';
 import 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyDMENOiJPd_288uGydhd7uKGiw2qPqsDyA',
-    authDomain: 'body-shop-a44ed.firebaseapp.com',
-    databaseURL: 'https://crud-a2bfe.firebaseio.com',
-    projectId: 'body-shop-a44ed',
-    storageBucket: 'body-shop-a44ed.appspot.com',
-    messagingSenderId:'519798548693',
-    appId: '1:519798548693:web:76169a57fe815bd9955a5c',
-    measurementId: 'G-MN2MYF6CER',
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId:'',
+    appId: '',
+    measurementId: '',
 };
 
 // Initialize Firebase
